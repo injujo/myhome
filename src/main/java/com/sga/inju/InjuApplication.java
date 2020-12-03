bbeb4ddbdf6a05545356ec959c10d862a99ff7ee
Jo In Ju <elwlahs280@naver.com>
2020-12-03 16:52:07 +0900

    init

---
+++ src/main/java/com/sga/inju/InjuApplication.java
@@ -0,0 +1,13 @@
+package com.sga.inju;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InjuApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InjuApplication.class, args);
+	}
+
+}
